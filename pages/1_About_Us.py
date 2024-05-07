39781d554b70e6bebba53ec4de2496ac00acb522
Comfort Os <56454834+Comfortti@users.noreply.github.com>
2024-05-07 09:33:18 +0100

    Update 1_About_Us.py

--- pages/1_About_Us.py
+++ pages/1_About_Us.py
@@ -5,8 +5,8 @@ def homepage():
     st.subheader("Let us introduce ourselves:")
     st.write("Chakaya is the brains behind this NLP code")
     st.write("Zofie is the whizz who primarily structured the report")
-    st.write("Anthony is the genius that helped put together this app")
-    st.write("Comfort is the Jack-of-All Trades, that helped in all sections")
+    st.write("Comfort is the genius that helped put together this app")
+    st.write("Anthony is the jack-of-all trades, that helped in all sections")
     st.image('teamwork.jpg')
 
 if __name__ == "__main__":
